a404fdd0cd1b90a23a48a68553f1d3e2329d344b
leondutoit <dutoit.leon@gmail.com>
2017-11-12 14:04:41 +0100

    implement calling api client admin functions

--- tsdapiclient/client.py
+++ tsdapiclient/client.py
@@ -2,13 +2,21 @@
 """Command-line interface to administrative tasks in API."""
 
 import json
+import sys
 
 import click
 import requests
+import yaml
 
 from config import PROD, TEST
 
 ENV = {'test': TEST, 'prod': PROD}
+
+
+def read_config(filename):
+    with open(filename) as f:
+        config = yaml.load(f)
+    return config
 
 
 def _post(url, endpoint, headers, data):
@@ -16,13 +24,19 @@ def _post(url, endpoint, headers, data):
     return resp
 
 
-def signup(env, pnum, client_name, email):
+def _check_present(_input, name):
+    if not _input:
+        print 'missing %s' % name
+        sys.exit(1)
+
+
+def do_signup(env, pnum, client_name, email):
     headers = {'Content-Type': 'application/json'}
     data = {'client_name': client_name, 'email': email}
     return _post(ENV[env], '/pnum/auth/basic/signup', headers, data)
 
 
-def confirm(env, pnum, client_id, confirmation_token):
+def do_confirm(env, pnum, client_id, confirmation_token):
     headers = {'Content-Type': 'application/json'}
     data = {'client_id': client_id, 'token': confirmation_token}
     return _post(ENV[env], '/pnum/auth/basic/confirm', headers, data)
@@ -51,36 +65,40 @@ def pw_reset(env, pnum, client_id, password):
 def print_guide():
     guide_text = """\
 
-        TSD API command line tool help
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        TSD API client line tool: tac
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
         Usage: tac --pnum <pnum> [OPTIONS]
 
-        Options:
-            --env           test (default) or prod.
-            --pnum          project number, e.g. p143.
-            --signup        register your client.
-            --confirm       confirm your client.
-            --getapikey     get a persistent API key.
-            --delapikey     revoke an API key.
-            --pwreset       reset your password.
-            --guide         print this guide.
+        Options help: tac --help
 
-        Registration guide
-        ~~~~~~~~~~~~~~~~~~
+        Registration
+        ~~~~~~~~~~~~
 
         Suppose you want to register for p11 in test:
 
-        tac --pnum p11 --signup --client_name <name> --email <email>
+        tac --pnum p11 --signup --client_name '<name>' --email '<email>'
         # returns a client_id
         # get your confirmation token in email
+        # save this in a config file:
+        # client_id: '<id>'
+        # confirmation_token: '<token>'
 
-        tac --pnum p11 --confirm --client_id <id> --confirmation_token <token>
+        tac --pnum p11 --confirm --config <file>
         # returns a password
         # ask for your client to be verified
+        # add this to the config file
+        # pass: '<pw>'
 
-        tac --pnum p11 --getapikey --client_id <id> --password <pw>
+        tac --pnum p11 --getapikey --config <file>
         # returns an API key
+        # save this in the config file
+        # api_key: <key>
+
+        Management
+        ~~~~~~~~~~
+        tac --pnum p11 --delapikey '<key>' --config <file>
+        tac --pnum p11 --pwreset --config <file>
 
         For more info please visit:
         test.api.tsd.usit.no/v1/docs/tsd-api-integration.html
@@ -95,21 +113,47 @@ def print_guide():
 @click.option('--signup', is_flag=True, default=False, help='register an API client')
 @click.option('--confirm', is_flag=True, default=False, help='confirm your details')
 @click.option('--getapikey', is_flag=True, default=False, help='get a persistent API key')
-@click.option('--delapikey', is_flag=True, default=False, help='revoke an API key')
+@click.option('--delapikey', default=False, help='revoke an API key')
 @click.option('--pwreset', is_flag=True, default=False, help='reset your password')
 @click.option('--guide', is_flag=True, default=False, help='print help text')
 @click.option('--client_name', default=None, help='your client\'s name')
 @click.option('--email', default=None, help='your email address')
-@click.option('--client_id', default=None, help='your client id')
-@click.option('--confirmation_token', default=None, help='your confirmation token')
-@click.option('--password', default=None, help='your API client password')
-@click.option('--api_key', default=None, help='your persistent API key')
+@click.option('--config', default=None, help='path to config file')
 def main(env, pnum, signup, confirm, getapikey, delapikey, pwreset, guide,
-         client_name, email, client_id, confirmation_token, password,
-         api_key):
+         client_name, email, config):
     if guide:
         print_guide()
-    return
+        return
+    if env not in ['test', 'prod']:
+        print 'unknown env'
+        sys.exit(1)
+    _check_present(env, 'env')
+    _check_present(pnum, 'pnum')
+    if signup:
+        _check_present(client_name, 'client_name')
+        _check_present(email, 'email')
+        do_signup(env, pnum, client_name, email)
+        return
+    if confirm:
+        _check_present(client_id, 'client_id')
+        _check_present(config, 'config')
+        config = read_config(config)
+        do_confirm(env, pnum, config['client_id'], config['confirmation_token'])
+        return
+    if getapikey:
+        _check_present(config, 'config')
+        config = read_config(config)
+        get_api_key(env, pnum, config['client_id'], config['pass'])
+    if delapikey:
+        _check_present(config, 'config')
+        config = read_config(config)
+        del_api_key(env, pnum, config['client_id'], config['pass'], delapikey)
+        return
+    if pwreset:
+        _check_present(config, 'config')
+        config = read_config(config)
+        pw_reset(env, pnum, config['client_id'], config['pass'])
+        return
 
 
 if __name__ == '__main__':
