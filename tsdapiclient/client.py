d3a335cb115f66604d570ae31b8b972ec60d094c
leondutoit <dutoit.leon@gmail.com>
2017-11-12 14:26:27 +0100

    print api responses, correct url construction

--- tsdapiclient/client.py
+++ tsdapiclient/client.py
@@ -19,9 +19,9 @@ def read_config(filename):
     return config
 
 
-def _post(url, endpoint, headers, data):
+def _post(url, headers, data):
     resp = requests.post(url, data=json.dumps(data), headers=headers)
-    return resp
+    return resp.text
 
 
 def _check_present(_input, name):
@@ -33,25 +33,32 @@ def _check_present(_input, name):
 def do_signup(env, pnum, client_name, email):
     headers = {'Content-Type': 'application/json'}
     data = {'client_name': client_name, 'email': email}
-    return _post(ENV[env], '/pnum/auth/basic/signup', headers, data)
+    url = '%s/%s/auth/basic/signup' % (ENV[env], pnum)
+    print 'POST: %s' % url
+    return _post(url, headers, data)
 
 
 def do_confirm(env, pnum, client_id, confirmation_token):
     headers = {'Content-Type': 'application/json'}
     data = {'client_id': client_id, 'token': confirmation_token}
-    return _post(ENV[env], '/pnum/auth/basic/confirm', headers, data)
+    url = '%s/%s/auth/basic/confirm' % (ENV[env], pnum)
+    print 'POST: %s' % url
+    return _post(url, headers, data)
 
 
 def get_api_key(env, pnum, client_id, password):
     headers = {'Content-Type': 'application/json'}
     data = {'client_id': client_id, 'pass': password}
-    return _post(ENV[env], '/pnum/auth/basic/api_key', headers, data)
+    url = '%s/%s/auth/basic/api_key' % (ENV[env], pnum)
+    print 'POST: %s' % url
+    return _post(url, headers, data)
 
 
 def del_api_key(env, pnum, client_id, password, api_key):
-    url = ENV[env] + '/pnum/auth/basic/signup'
     headers = {'Content-Type': 'application/json'}
     data = {'client_id': client_name, 'pass': password, 'api_key': api_key}
+    url = '%s/%s/auth/basic/api_key' % (ENV[env], pnum)
+    print 'DELETE: %s' % url
     resp = requests.delete(url, data=json.dumps(data), headers=headers)
     return resp.text
 
@@ -59,7 +66,9 @@ def del_api_key(env, pnum, client_id, password, api_key):
 def pw_reset(env, pnum, client_id, password):
     headers = {'Content-Type': 'application/json'}
     data = {'client_id': client_id, 'pass': password}
-    return _post(ENV[env], '/pnum/auth/basic/reset_password', headers, data)
+    url = '%s/%s/auth/basic/reset_password' % (ENV[env], pnum)
+    print 'POST: %s' % url
+    return _post(url, headers, data)
 
 
 def print_guide():
@@ -132,27 +141,27 @@ def main(env, pnum, signup, confirm, getapikey, delapikey, pwreset, guide,
     if signup:
         _check_present(client_name, 'client_name')
         _check_present(email, 'email')
-        do_signup(env, pnum, client_name, email)
+        print do_signup(env, pnum, client_name, email)
         return
     if confirm:
         _check_present(client_id, 'client_id')
         _check_present(config, 'config')
         config = read_config(config)
-        do_confirm(env, pnum, config['client_id'], config['confirmation_token'])
+        print do_confirm(env, pnum, config['client_id'], config['confirmation_token'])
         return
     if getapikey:
         _check_present(config, 'config')
         config = read_config(config)
-        get_api_key(env, pnum, config['client_id'], config['pass'])
+        print get_api_key(env, pnum, config['client_id'], config['pass'])
     if delapikey:
         _check_present(config, 'config')
         config = read_config(config)
-        del_api_key(env, pnum, config['client_id'], config['pass'], delapikey)
+        print del_api_key(env, pnum, config['client_id'], config['pass'], delapikey)
         return
     if pwreset:
         _check_present(config, 'config')
         config = read_config(config)
-        pw_reset(env, pnum, config['client_id'], config['pass'])
+        print pw_reset(env, pnum, config['client_id'], config['pass'])
         return
 
 
