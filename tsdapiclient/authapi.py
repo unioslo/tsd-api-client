730bc43a20c73d904533738147a91915a4b8c617
leondutoit <dutoit.leon@gmail.com>
2017-11-14 11:48:14 +0100

    add basic exception handling for auth methods

--- tsdapiclient/authapi.py
+++ tsdapiclient/authapi.py
@@ -10,7 +10,10 @@ def get_jwt_basic_auth(env, pnum, api_key):
     headers = {'Content-Type': 'application/json',
                'Authorization': 'Bearer ' + api_key}
     url = '%s/%s/auth/basic/token' % (ENV[env], pnum)
-    resp = requests.post(url, headers=headers)
+    try:
+        resp = requests.post(url, headers=headers)
+    except Exception:
+        return None
     if resp.status_code in [200, 201]:
         token = json.loads(resp.text)['token']
         return None
@@ -23,7 +26,10 @@ def get_jwt_tsd_auth(env, pnum, api_key, user_name, password, otp, token_type):
                'Authorization': 'Bearer ' + api_key}
     data = {'user_name': user_name, 'password': password, 'otp': otp}
     url = '%s/%s/auth/tsd/token' % (ENV[env], pnum)
-    resp = requests.post(url, data=json.dumps(data), headers=headers)
+    try:
+        resp = requests.post(url, data=json.dumps(data), headers=headers)
+    except Exception:
+        return None
     if resp.status_code in [200, 201]:
         token = json.loads(resp.text)['token']
         return None
