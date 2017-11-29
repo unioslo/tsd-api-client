244fdab75745b7b47227652d74d17b9f1e61623d
leondutoit <dutoit.leon@gmail.com>
2017-11-29 09:03:23 +0100

    bump version to 0.7.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='tsd-api-client',
-    version='0.6.0',
+    version='0.7.0',
     description='A client for the TSD REST API',
     author='Leon du Toit',
     author_email='l.c.d.toit@usit.uio.no',
