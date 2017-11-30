ffe966ef0fa80b8b4d19910acea6dc5fa97d1fe5
leondutoit <dutoit.leon@gmail.com>
2017-11-30 13:30:54 +0100

    implement using built-in api keys (with TSD 2FA)

--- tsdapiclient/config.py
+++ tsdapiclient/config.py
@@ -3,3 +3,7 @@ VERSION = 'v1'
 PROD = 'https://api.tsd.usit.no' + '/' + VERSION
 TEST = 'https://test.api.tsd.usit.no' + '/' + VERSION
 ENV = {'test': TEST, 'prod': PROD}
+API_KEYS = {
+    'test': '',
+    'prod': ''
+}
