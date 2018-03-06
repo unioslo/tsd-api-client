22257e7f1b00ea8b8316763d6d5eb0e438f4ae81
leondutoit <dutoit.leon@gmail.com>
2018-03-06 13:53:53 +0100

    allow updating project specific api keys

--- tsdapiclient/configurer.py
+++ tsdapiclient/configurer.py
@@ -2,11 +2,13 @@
 """Module for managing tacl config."""
 
 import os
+import re
 
 import yaml
 
 HOME = os.path.expanduser('~')
 TACL_CONFIG = HOME + '/.tacl_config'
+
 
 def read_config(filename=TACL_CONFIG):
     with open(filename, 'rw') as f:
@@ -36,17 +38,18 @@ def update_config(env, key, val):
         if not new_env.has_key(key):
             print 'updating %s' % key
             new_config[env].update({key:val})
-        # TODO
-        # need abilty to update pXX
         elif key in ['api_key', 'pass']:
             print 'updating %s' % key
             new_config[env].update({key:val})
-        elif new_env.has_key(key):
+        elif key in ['client_id', 'email', 'client_name']:
             print 'trying to modify %s - not allowed' % key
             print 'if you want to do that, delete your current config'
             print 'and register again'
             write_config(config, TACL_CONFIG)
             return
+        else:
+            print 'updating %s' % key
+            new_config[env].update({key:val})
         write_config(new_config, TACL_CONFIG)
     except IOError:
         new_config = {}
