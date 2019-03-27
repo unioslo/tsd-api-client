4ae5a18f56f9ba19d91a7329dfbbc675d2ff3355
leondutoit <dutoit.leon@gmail.com>
2019-03-27 09:41:13 +0100

    add user agent generator function

--- tsdapiclient/tools.py
+++ tsdapiclient/tools.py
@@ -1,9 +1,22 @@
 
 """Diverse helpers."""
 
+import os
 import sys
+import hashlib
+
 
 def _check_present(_input, name):
     if not _input:
         print 'missing %s' % name
         sys.exit(1)
+
+
+def user_agent(name='tsd-api-client'):
+    version = '1.7.1'
+    try:
+        user = os.environ.get('USER')
+    except (Exception, OSError) as e:
+        user = 'not-found'
+    hu = hashlib.md5(user).hexdigest()
+    return '%s-%s-%s' % (name, version, hu)
