ed7976bc3dbddede3a1c7aeef556cf2835e3de95
leondutoit <dutoit.leon@gmail.com>
2019-06-05 15:06:38 +0200

    bump version in src

--- tsdapiclient/tools.py
+++ tsdapiclient/tools.py
@@ -16,7 +16,7 @@ def _check_present(_input, name):
 
 
 def user_agent(name='tsd-api-client'):
-    version = '1.8.2'
+    version = '1.8.3'
     try:
         user = os.environ.get('USER')
     except (Exception, OSError) as e:
