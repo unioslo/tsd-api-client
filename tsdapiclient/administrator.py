1373a35496e4b6fa58cbfb6e9363d8efdb2e68dd
leondutoit <dutoit.leon@gmail.com>
2019-09-19 16:14:33 +0200

    formatting in administrator module

--- tsdapiclient/administrator.py
+++ tsdapiclient/administrator.py
@@ -16,32 +16,32 @@ def _post(url, headers, data):
 def do_signup(env, pnum, client_name, email):
     headers = {'Content-Type': 'application/json'}
     data = {'client_name': client_name, 'email': email}
-    url = '%s/%s/auth/basic/signup' % (ENV[env], pnum)
-    print 'POST: %s' % url
+    url = '{0}/{1}/auth/basic/signup' % (ENV[env], pnum)
+    print('POST: {0}'.format(url))
     return _post(url, headers, data)
 
 
 def do_confirm(env, pnum, client_id, confirmation_token):
     headers = {'Content-Type': 'application/json'}
     data = {'client_id': client_id, 'token': confirmation_token}
-    url = '%s/%s/auth/basic/confirm' % (ENV[env], pnum)
-    print 'POST: %s' % url
+    url = '{0}/{1}/auth/basic/confirm' % (ENV[env], pnum)
+    print('POST: {0}'.format(url))
     return _post(url, headers, data)
 
 
 def get_api_key(env, pnum, client_id, password):
     headers = {'Content-Type': 'application/json'}
     data = {'client_id': client_id, 'pass': password}
-    url = '%s/%s/auth/basic/api_key' % (ENV[env], pnum)
-    print 'POST: %s' % url
+    url = '{0}/{1}/auth/basic/api_key' % (ENV[env], pnum)
+    print('POST: {0}'.format(url))
     return _post(url, headers, data)
 
 
 def del_api_key(env, pnum, client_id, password, api_key):
     headers = {'Content-Type': 'application/json'}
     data = {'client_id': client_name, 'pass': password, 'api_key': api_key}
-    url = '%s/%s/auth/basic/api_key' % (ENV[env], pnum)
-    print 'DELETE: %s' % url
+    url = '{0}/{1}/auth/basic/api_key' % (ENV[env], pnum)
+    print('DELETE: {0}'.format(url))
     resp = requests.delete(url, data=json.dumps(data), headers=headers)
     return json.loads(resp.text)
 
@@ -49,15 +49,15 @@ def del_api_key(env, pnum, client_id, password, api_key):
 def pw_reset(env, pnum, client_id, password):
     headers = {'Content-Type': 'application/json'}
     data = {'client_id': client_id, 'pass': password}
-    url = '%s/%s/auth/basic/reset_password' % (ENV[env], pnum)
-    print 'POST: %s' % url
+    url = '{0}/{1}/auth/basic/reset_password' % (ENV[env], pnum)
+    print('POST: {0}'.format(url))
     return _post(url, headers, data)
 
 
 def get_tsd_api_key(env, pnum, user_name, password, otp):
     headers = {'Content-Type': 'application/json'}
     data = {'user_name': user_name, 'password': password, 'otp': otp}
-    url = '%s/%s/auth/tsd/api_key' % (ENV[env], pnum)
-    print 'GET: %s' % url
+    url = '{0}/{1}/auth/tsd/api_key' % (ENV[env], pnum)
+    print('GET: {0}'.format(url))
     resp = requests.get(url, headers=headers, data=json.dumps(data))
     return json.loads(resp.text)['api_key']
