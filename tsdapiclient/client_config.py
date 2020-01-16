7dec92a588df64a23d921f24e150b1bf787cf92d
leondutoit <dutoit.leon@gmail.com>
2020-01-16 12:27:26 +0100

    support registering for alt.api.tsd.usit.no, improve the ux a bit

--- tsdapiclient/client_config.py
+++ tsdapiclient/client_config.py
@@ -4,4 +4,5 @@
 API_VERSION = 'v1'
 PROD = 'https://api.tsd.usit.no/{0}'.format(API_VERSION)
 TEST = 'https://test.api.tsd.usit.no/{0}'.format(API_VERSION)
-ENV = {'test': TEST, 'prod': PROD}
+ALT = 'https://alt.api.tsd.usit.no/{0}'.format(API_VERSION)
+ENV = {'test': TEST, 'prod': PROD, 'alt': ALT}
