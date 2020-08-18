c5578d00e54af6de2ca47c1f20a44371f2d04a3e
leondutoit <dutoit.leon@gmail.com>
2020-08-18 12:27:45 +0200

    define chunk options as config constants

--- tsdapiclient/client_config.py
+++ tsdapiclient/client_config.py
@@ -6,3 +6,5 @@ PROD = 'https://api.tsd.usit.no/{0}'.format(API_VERSION)
 TEST = 'https://test.api.tsd.usit.no/{0}'.format(API_VERSION)
 ALT = 'https://alt.api.tsd.usit.no/{0}'.format(API_VERSION)
 ENV = {'test': TEST, 'prod': PROD, 'alt': ALT}
+CHUNK_THRESHOLD = 1000*1000*1000 # 1gb
+CHUNK_SIZE = 1000*1000*50 # 50mb
