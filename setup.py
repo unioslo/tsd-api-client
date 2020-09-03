af2865eae538245595a29e3e9d8c4b70dbec6aa7
leondutoit <dutoit.leon@gmail.com>
2020-09-03 12:38:53 +0200

    replace tacl with tacl2, bump version to v3.0.0

--- setup.py
+++ setup.py
@@ -13,23 +13,16 @@ setup(
     author_email='l.c.d.toit@usit.uio.no',
     url='https://github.com/unioslo/tsd-api-client',
     packages=['tsdapiclient'],
-    scripts=[
-        'scripts/tacl',
-        'scripts/tacl_admin',
-        'scripts/tacl_data',
-        'scripts/tacl_auth',
-        'scripts/tsd-s3cmd'],
     install_requires = [
         'requests',
         'click',
         'PyYAML',
         'progress',
         'humanfriendly',
-        's3cmd @ https://github.com/unioslo/s3cmd/archive/v2.1.0-custom-headers.tar.gz#egg=s3cmd-2.1.0-custom-headers'
     ],
     python_requires='>=3.6',
     entry_points='''
         [console_scripts]
-        tacl2=tsdapiclient.tacl2:cli
+        tacl=tsdapiclient.tacl:cli
     ''',
 )
--- tsdapiclient/guide.py
+++ tsdapiclient/guide.py
@@ -5,13 +5,14 @@ topics = """
 config
 uploads
 downloads
+automation
 debugging
 """
 
 config = """
-To use tacl2, you first need to register with the TSD API:
+To use tacl, you first need to register with the TSD API:
 
-    tacl2 --register
+    tacl --register
 
 A registration lasts one year, after which you need to register
 again. If you are a member of different TSD projects, you need to
@@ -19,71 +20,89 @@ register separately for each project.
 
 For an overview of current registrations:
 
-    tacl2 --config-show
+    tacl --config-show
 
 To delete all existing config:
 
-    tacl2 --config-delete
+    tacl --config-delete
 
 """
 
 uploads = """
 Upload a single file:
 
-    tacl2 p11 --upload myfile.txt
+    tacl p11 --upload myfile.txt
 
 Files larger than 1GB are resumable if something goes wrong:
 
-    tacl2 p11 --upload myfile.txt --upload-id 52928fed-8c29-4135-88e9-27f2c0bec526
+    tacl p11 --upload myfile.txt --upload-id 52928fed-8c29-4135-88e9-27f2c0bec526
 
 To browse and manage resumables:
 
-    tacl2 p11 --resume-list
-    tacl2 p11 --resume-delete 52928fed-8c29-4135-88e9-27f2c0bec526
-    tacl2 p11 --resume-delete-all
+    tacl p11 --resume-list
+    tacl p11 --resume-delete 52928fed-8c29-4135-88e9-27f2c0bec526
+    tacl p11 --resume-delete-all
 
 Uploading directories (automatically resumable for the whole directory):
 
-    tacl2 p11 --upload mydirectory
+    tacl p11 --upload mydirectory
 
 To control which folders and files are included:
 
-    tacl2 p11 --upload mydirectory --ignore-prefixes .git,build,dist --ignore-suffixes .pyc,.db
+    tacl p11 --upload mydirectory --ignore-prefixes .git,build,dist --ignore-suffixes .pyc,.db
 
 To disable the resume functionality for a directory:
 
-    tacl2 p11 --upload mydirectory --cache-disable
+    tacl p11 --upload mydirectory --cache-disable
 
 To view and manage directory upload cache:
 
-    tacl2 p11 --upload-cache-show
-    tacl2 p11 --upload-cache-delete mydirectory
-    tacl2 p11 --upload-cache-delete-all
+    tacl p11 --upload-cache-show
+    tacl p11 --upload-cache-delete mydirectory
+    tacl p11 --upload-cache-delete-all
 
 """
 
 downloads = """
 To view files and folders available for download:
 
-    tacl2 p11 --download-list
+    tacl p11 --download-list
 
 Download a file:
 
-    tacl2 p11 --download anonymised-sensitive-data.txt
+    tacl p11 --download anonymised-sensitive-data.txt
 
 If something goes wrong during the download, resume it:
 
-    tacl2 p11 --download anonymised-sensitive-data.txt --download-id 869b432d7703e62134fcca775c98ba38
+    tacl p11 --download anonymised-sensitive-data.txt --download-id 869b432d7703e62134fcca775c98ba38
 
 To download a directory (resumable):
 
-    tacl2 p11 --download mydir --ignore-prefixes mydir/.git
+    tacl p11 --download mydir --ignore-prefixes mydir/.git
 
 To view and manage the directory download cache:
 
-    tacl2 p11 --download-cache-show
-    tacl2 p11 --download-cache-delete mydir
-    tacl2 p11 --download-cache-delete-all
+    tacl p11 --download-cache-show
+    tacl p11 --download-cache-delete mydir
+    tacl p11 --download-cache-delete-all
+
+"""
+
+automation = f"""
+To import data to your TSD project(s) in an automated way,
+you firstly need to organise access for your machine(s) to the TSD API,
+by proividing an IP address/range. If your machine(s) are located on
+an Uninett network, then TSD's current security policy allows
+enabling access without issue.
+
+If not, then you will have to provide a description of how you
+secure your machine(s) and include it in the risk analysis of your project.
+
+Given that this level of access is taken care of, that you have registered tacl,
+and that you have obtained TSD credentials for your project(s),
+you can set up automated data import in the following way:
+
+tacl p11 --basic --upload myfile
 
 """
 
@@ -91,8 +110,8 @@ debugging = f"""
 If you are having trouble while running a command, check the version,
 and run the problematic command in verbose mode:
 
-    tacl2 --version
-    tacl2 p11 --verbose --upload myfile # e.g.
+    tacl --version
+    tacl p11 --verbose --upload myfile # e.g.
 
 Take contact with TSD, sending the output: {HELP_URL}
 
