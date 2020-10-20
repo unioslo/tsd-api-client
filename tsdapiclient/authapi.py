42c8463bc467c6e349a6c68c6ee7e5a27c9cd5ee
leondutoit <dutoit.leon@gmail.com>
2020-10-20 12:43:45 +0200

    new url construction in authapi module

--- tsdapiclient/authapi.py
+++ tsdapiclient/authapi.py
@@ -5,13 +5,15 @@ import json
 import requests
 
 from tsdapiclient.client_config import ENV
-from tsdapiclient.tools import handle_request_errors
+from tsdapiclient.tools import handle_request_errors, auth_api_url
 
 @handle_request_errors
-def get_jwt_basic_auth(env, pnum, api_key):
-    headers = {'Content-Type': 'application/json',
-               'Authorization': 'Bearer {0}'.format(api_key)}
-    url = '{0}/{1}/auth/basic/token'.format(ENV[env], pnum)
+def get_jwt_basic_auth(env, pnum, api_key, token_type='import'):
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_key}'
+    }
+    url = f'{auth_api_url(env, pnum, "basic")}?type={token_type}'
     try:
         resp = requests.post(url, headers=headers)
     except Exception as e:
@@ -23,12 +25,25 @@ def get_jwt_basic_auth(env, pnum, api_key):
         return None
 
 @handle_request_errors
-def get_jwt_tsd_auth(env, pnum, api_key, user_name, password,
-                     otp, token_type):
-    headers = {'Content-Type': 'application/json',
-               'Authorization': 'Bearer {0}'.format(api_key)}
-    data = {'user_name': user_name, 'password': password, 'otp': otp}
-    url = '{0}/{1}/auth/tsd/token?type={2}'.format(ENV[env], pnum, token_type)
+def get_jwt_tsd_auth(
+    env,
+    pnum,
+    api_key,
+    user_name,
+    password,
+    otp,
+    token_type
+):
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_key}'
+    }
+    data = {
+        'user_name': user_name,
+        'password': password,
+        'otp': otp
+    }
+    url = f'{auth_api_url(env, pnum, "tsd")}?type={token_type}'
     try:
         resp = requests.post(url, data=json.dumps(data), headers=headers)
     except Exception as e:
