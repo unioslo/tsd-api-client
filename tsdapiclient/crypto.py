0e0bc8f467af4930ffd8de0c9ce6011f7eafc9e5
leondutoit <dutoit.leon@gmail.com>
2021-12-15 14:28:34 +0100

    add module with libsodium helper functions

---
+++ tsdapiclient/crypto.py
@@ -0,0 +1,41 @@
+
+"""Wrapper functions to encapsulate libsodium and API details."""
+
+import base64
+import json
+
+import libnacl
+import libnacl.sealed
+import libnacl.public
+import libnacl.utils
+import requests
+
+from tsdapiclient.tools import HOSTS
+
+
+def nacl_encrypt_data(data: bytes, nonce: bytes, key: bytes) -> bytes:
+    return libnacl.crypto_stream_xor(data, nonce, key)
+
+
+def nacl_gen_nonce() -> bytes:
+    return libnacl.utils.rand_nonce()
+
+
+def nacl_gen_key() -> bytes:
+    return libnacl.utils.salsa_key()
+
+
+def nacl_get_server_public_key(env: str, pnum: str) -> bytes:
+    host = HOSTS.get(env)
+    resp = requests.get(f'https://{host}/v1/{pnum}/files/crypto/key')
+    encoded_public_key = json.loads(resp.text).get('public_key')
+    return libnacl.public.PublicKey(base64.b64decode(encoded_public_key))
+
+
+def nacl_encrypt_header(public_key: bytes, header: bytes) -> bytes:
+    sbox = libnacl.sealed.SealedBox(public_key)
+    return sbox.encrypt(header)
+
+
+def nacl_encode_header(header: bytes) -> str:
+    return base64.b64encode(header)
