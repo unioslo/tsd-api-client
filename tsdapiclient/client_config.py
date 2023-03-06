58906e3065e91ed5c0087aae9ad8d79aba2a9a0d
Milen Kouylekov <milen.kouylekov@usit.uio.no>
2023-03-06 14:45:49 +0100

    New Test EC url

--- tsdapiclient/client_config.py
+++ tsdapiclient/client_config.py
@@ -6,7 +6,7 @@ PROD = 'https://api.tsd.usit.no/{0}'.format(API_VERSION)
 TEST = 'https://test.api.tsd.usit.no/{0}'.format(API_VERSION)
 ALT = 'https://alt.api.tsd.usit.no/{0}'.format(API_VERSION)
 EC_PROD = 'https://api.fp.educloud.no/{0}'.format(API_VERSION)
-EC_TEST = 'https://test-api-educloud.uio.no/{0}'.format(API_VERSION)
+EC_TEST = 'https://test.api.fp.educloud.no/{0}'.format(API_VERSION)
 DEV = 'http://localhost:3003/{0}'.format(API_VERSION)
 ENV = {
     'test': TEST,
